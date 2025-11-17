4ee93570fd500547934c48be3fd4ce9481fd9b62
Tushar Mandiwal <dhirajmon00@gmail.com>
2025-11-17 22:09:45 +0530

    Add backend microservices of Pharmacy Management System

---
+++ pharmacy_backend/Order-Service/src/main/java/com/pharmacy/Order_Service/OrderServiceApplication.java
@@ -0,0 +1,14 @@
+package com.pharmacy.Order_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@EnableFeignClients
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
